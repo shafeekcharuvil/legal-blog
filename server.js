79175daadcea61e58d4f2ed84512105ff32f9d8f
shafeekcharuvil <shafeekcharuvilakamparamba@gmail.com>
2025-04-09 14:52:30 +0400

    Updated backend URL

--- server.js
+++ server.js
@@ -20,4 +20,4 @@ app.use("/api", require("./routes/auth"));
 app.use("/api/posts", require("./routes/posts"));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(Server running on port ${PORT}));
+app.listen(PORT, () => console.log('Server running on port ${PORT}'));
